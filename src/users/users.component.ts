ded9ebfd17c287c9f4bda14f79a29346b469726a
Casper Knudsen <casper60@gmail.com>
2016-05-22 21:40:36 +0200

    Removed internal bindings and exposed service observables instead

--- src/users/users.component.ts
+++ src/users/users.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs/Rx';
 
 import { AppStoreService, AppStore } from '../app/app.store';
-import { User } from './service/user.service';
-
 import { UserListComponent } from './userlist.component';
 import { UserFormComponent } from './userform.component';
 
@@ -14,22 +11,15 @@ import { UserFormComponent } from './userform.component';
   selector: 'users',
   template: `
   <h3>Users</h3>
-  <user-list [users]="users$ | async"></user-list>
-  <user-form [user]="user$ | async"></user-form>
+  <user-list [users]="appStore.users.users$ | async"></user-list>
+  <user-form [user]="appStore.users.selectedUser$ | async"></user-form>
   `,
   directives: [UserListComponent, UserFormComponent]
 })
 export class UsersComponent {
-  users$: Observable<User[]>;
-  user$: Observable<User>;
 
   constructor(private appStore: AppStoreService, private store: Store<AppStore>) {
-    let users = this.appStore.users;
-
-    this.users$ = users.users$;
-    this.user$ = users.selectedUser$;
-
-    users.fetch();
+    this.appStore.users.fetch();
   }
 
 }
